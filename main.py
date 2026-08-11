eccb5e8b319966c5996dd637b090fe7ac0f7128b
SDakota605 <blakereimnitz@gmail.com>
2026-08-11 18:27:22 -0400

    added circle shape and finished the clock in main.py

--- main.py
+++ main.py
@@ -9,7 +9,13 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
 
+
+
     pygame.init()
+
+    clock = pygame.time.Clock()
+    dt = 0.0
+
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
@@ -19,9 +25,12 @@ def main():
             pass
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+
 
 
 
